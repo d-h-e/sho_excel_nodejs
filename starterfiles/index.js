69eae7acd3e1777aac9a5689c52f5f3d6a39b4c3
d-h-e <dhe@internetschmie.de>
2019-09-19 09:53:53 +0200

    alphabet for columns

--- starterfiles/index.js
+++ starterfiles/index.js
@@ -4,18 +4,20 @@ const fs = require('fs');
 const file = xlsx.readFile('./EXCEL-FILE/sho_excel.xlsx');
 const sheet_name_list = file.SheetNames;
 const sheet = file.Sheets[sheet_name_list[0]];
-// const sheet = file.Sheets['sheetname'];
+
+const alphabet = [...Array(26)].map((_, i) => String.fromCharCode('A'.charCodeAt(0) + i));
+const col = (char) => alphabet.indexOf(char);
 
 const data = xlsx.utils.sheet_to_json(sheet, { header: "A" });
 
 data.forEach(item => {
     let valueArr = Object.values(item);
 
-    let fileName = valueArr[0] + '.md';
+    let fileName = valueArr[col('A')] + '.md';
 
-    let fileContent = '#' + valueArr[1] + '   \n';
-    fileContent += valueArr[2] + '   \n';
-    fileContent += valueArr[3] + '   \n';
+    let fileContent = '#' + valueArr[col('B')] + '   \n';
+    fileContent += valueArr[col('C')] + '   \n';
+    fileContent += valueArr[col('D')] + '   \n';
 
     console.log(fileContent);
 
